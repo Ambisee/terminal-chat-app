929f9c1fae96c0288b41123d7543e51b995a1ef4
Ambisee <celvin.weilington@gmail.com>
2022-08-11 18:23:09 -0700

    Reworked the run_server.py file

--- run_server.py
+++ run_server.py
@@ -4,16 +4,20 @@ Driver code that runs the server on a specific port
 '''
 # --- Modules --- #
 import sys
+from typing import List
 
 from terminal_chat_app.server import Server
 
 # --- Main Function --- #
 if __name__ == '__main__':
-    arguments = sys.argv
-    port = arguments[1].split(':') if len(arguments) > 1 else None
+    arguments: List = sys.argv
+    if len(arguments) > 1:
+        port: List[str] = arguments[1].split(':')
+    else:
+        port: List[str] = []
     
     try:
-        if port:
+        if len(port) == 2:
             server = Server(port[0], int(port[1]))
         else:
             server = Server()
